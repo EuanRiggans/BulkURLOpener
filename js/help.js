93026e71c2819fe2205f9a4eaab2dcc3f9d38c23
Euan <euanriggans1@gmail.com>
2018-07-19 23:53:30 +0100

    Added help entries for new features

--- js/help.js
+++ js/help.js
@@ -11,6 +11,9 @@ $(document).ready(function () {
     $('#openSupportURL').click(function () {
         chrome.tabs.create({'url': "https:chrome.google.com/webstore/detail/"+chrome.runtime.id+"/support"});
     });
+    $('#experimentalReport').click(function () {
+        chrome.tabs.create({'url': "https:chrome.google.com/webstore/detail/" + chrome.runtime.id + "/support"});
+    });
     $('#openSupportURLFooter').click(function () {
         chrome.tabs.create({'url': "https:chrome.google.com/webstore/detail/"+chrome.runtime.id+"/support"});
     });
