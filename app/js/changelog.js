d6211d7ea78788bd684bef2bd61de64761f028fc
Euan Riggans <euan@euanriggans.com>
2020-02-04 22:24:11 +0000

    Removed changelog.js dependence on Jquery

--- app/js/changelog.js
+++ app/js/changelog.js
@@ -21,7 +21,7 @@
     });
 })();
 
-$('#closeModal').click(function () {
+document.getElementById('closeModal').addEventListener('click', () => {
     if (checkHostType() === "firefox") {
         alert("Unable to close window due to Firefox security policy. Please close this window manually.");
         // window.close();
