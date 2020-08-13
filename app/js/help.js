9fac73359813b9ca56980d40435d901d53a8a0d4
Euan Riggans <euan@euanriggans.com>
2020-08-13 23:05:18 +0100

    Added more cards to help page.
    - Added: view changelog, view source code and check for updates (not yet implemented) buttons.

--- app/js/help.js
+++ app/js/help.js
@@ -10,11 +10,22 @@ document.getElementById('openFAQ').addEventListener('click', () => {
     openFAQ();
 });
 document.getElementById('openGithubBug').addEventListener('click', () => {
-    openGithub();
+    openGithubIssues();
 });
 document.getElementById('openGithubFeature').addEventListener('click', () => {
+    openGithubIssues();
+});
+document.getElementById('openSourceCode').addEventListener('click', () => {
     openGithub();
 });
+document.getElementById('openChangelog').addEventListener('click', () => {
+    openChangelog();
+});
+document.getElementById('checkForUpdates').addEventListener('click', () => {
+    checkForUpdates();
+});
+
+/* End Of Event Listeners */
 
 function openFAQ() {
     if (checkHostType() === "firefox") {
@@ -31,13 +42,28 @@ function openFAQ() {
     }
 }
 
+function openChangelog() {
+    if (checkHostType() === "firefox") {
+        browser.tabs.create({
+            active: true,
+            'url': browser.extension.getURL('changelog.html')
+        });
+    } else if (checkHostType() === "chrome") {
+        chrome.tabs.create({
+            'url': chrome.extension.getURL('changelog.html')
+        });
+    } else if (checkHostType() === "electron") {
+        window.location.replace('changelog.html');
+    }
+}
+
 function openGithub() {
     const githubURL = "https://euan.link/buo-github";
     if (checkHostType() === "electron") {
         const {
             shell
         } = require('electron');
-        shell.openExternal("https://euan.link/buo-github");
+        shell.openExternal(githubURL);
     } else if (checkHostType() === "firefox") {
         browser.tabs.create({
             active: true,
@@ -48,4 +74,27 @@ function openGithub() {
             'url': githubURL
         });
     }
+}
+
+function openGithubIssues() {
+    const githubURL = "https://euan.link/buo-issues";
+    if (checkHostType() === "electron") {
+        const {
+            shell
+        } = require('electron');
+        shell.openExternal(githubURL);
+    } else if (checkHostType() === "firefox") {
+        browser.tabs.create({
+            active: true,
+            'url': githubURL
+        });
+    } else if (checkHostType() === "chrome") {
+        chrome.tabs.create({
+            'url': githubURL
+        });
+    }
+}
+
+function checkForUpdates() {
+    alert("@todo Add this feature")
 }
