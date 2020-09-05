a29f604855199f2f5308ad7df04eaaf5e4a30c9c
Euan Riggans <euan@euanriggans.com>
2020-09-05 22:57:09 +0100

    Added option to manually sync data to browser storage:
    - Added option for browser based users to sync their data to the browsers storage. Currently a manual action, so data is not automatically synced.
    - Closes #35

--- app/pages/help/help.js
+++ app/pages/help/help.js
@@ -22,8 +22,82 @@ document.getElementById("openSourceCode").addEventListener("click", () => {
 document.getElementById("openChangelog").addEventListener("click", () => {
     openChangelog();
 });
+
 document.getElementById("checkForUpdates").addEventListener("click", () => {
     checkForUpdates();
+});
+
+document.getElementById("syncToBrowser").addEventListener("click", () => {
+    let exportData = {
+        maxID: 0,
+        lists: [],
+        settings: {},
+    };
+    for (let i = 0; i < localStorage.length; i++) {
+        const tempStorage = loadList(localStorage.key(i));
+        const parsedJSON = JSON.parse(tempStorage);
+        if (localStorage.key(i) === "settings") {
+            exportData.settings = parsedJSON;
+        } else if (parsedJSON.object_description === "list_storage") {
+            exportData.lists.push(parsedJSON);
+        } else if (localStorage.getItem("maxID") !== "NaN") {
+            exportData.maxID = localStorage.getItem("maxID");
+        }
+    }
+    exportData = JSON.stringify(exportData);
+    chrome.storage.sync.set({"user_settings": exportData}, () => {
+        alert("Successfully saved data to browser storage.");
+    });
+});
+
+document.getElementById("syncFromBrowser").addEventListener("click", () => {
+    chrome.storage.sync.get(function (result) {
+        let syncedData;
+        try {
+            syncedData = JSON.parse(result.user_settings);
+        } catch (e) {
+            console.log(e);
+            alert("Failed to get data from browser storage. You may not have anything data stored.");
+            return;
+        }
+        // @todo Add visualisation for the user, show them what data is coming from the sync, so they can decide.
+        if (confirm(
+            "This will overwrite all of your current settings (Lists, settings etc) with the data synced to " +
+            "our browsers storage. Are you sure you wish to continue?"
+        )) {
+            let skipLists = false;
+            let skipSettings = false;
+            if (syncedData.maxID === undefined || syncedData.lists === undefined) {
+                skipLists = true;
+                if (!confirm("Your imported data is missing lists or lists max ID. Do you wish to continue with importing this data?")) {
+                    return;
+                }
+            }
+            if (syncedData.settings === undefined) {
+                skipSettings = true;
+                if (!confirm("Your imported data is missing settings. Do you wish to continue with importing this data?")) {
+                    return;
+                }
+            }
+            localStorage.clear();
+            let maxID = 0;
+            let userLists = [];
+            let userSettings;
+            if (!skipLists) {
+                maxID = syncedData.maxID;
+                userLists = syncedData.lists;
+            }
+            if (!skipSettings) {
+                userSettings = syncedData.settings;
+            }
+            localStorage.setItem("maxID", maxID);
+            for (const list of userLists) {
+                localStorage.setItem(list.list_id, JSON.stringify(list));
+            }
+            localStorage.setItem("settings", JSON.stringify(userSettings));
+            alert("Successfully imported settings.");
+        }
+    });
 });
 
 if (document.getElementById("goHome")) document.getElementById("goHome").addEventListener("click", goHome);
